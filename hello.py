8c722a9cd9c62b0505f6463ed011b9941e3027d7
Eric Ji <ericji1326@gmail.com>
2024-09-23 00:31:28 -0400

    replay and modify example 4-7 in chapter 4

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -11,9 +11,9 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your email?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -30,10 +30,21 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+    email_message = None
+
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+
+    if session.get('email'):
+        if 'utoronto' not in session['email']:
+            email_message = "Please use your UofT email."
+    
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), email_message=email_message)
